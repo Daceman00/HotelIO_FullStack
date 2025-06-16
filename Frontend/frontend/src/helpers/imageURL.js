14495f90e2748d19bafa2c215224f375e1fa126c
Daceman00 <dariomandic2000@gmail.com>
2025-06-16 22:43:45 +0200

    Refactor code structure for improved readability and maintainability

--- Frontend/frontend/src/helpers/imageURL.js
+++ Frontend/frontend/src/helpers/imageURL.js
@@ -1,3 +1,3 @@
 export const IMAGE_URL = "http://localhost:3000/rooms";
 export const IMAGE_URL_USERS = "http://localhost:3000/users";
-export const IMAGE_URL_MENU = "http://localhost:5173";
+export const IMAGE_URL_MENU = "http://localhost:5173/public";
