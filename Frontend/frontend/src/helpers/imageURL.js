b4dcd660bae15ec7d562ea3d389c3025887ffc33
Daceman00 <dariomandic2000@gmail.com>
2025-06-17 15:43:07 +0200

    fix: update image URLs to remove localhost references

--- Frontend/frontend/src/helpers/imageURL.js
+++ Frontend/frontend/src/helpers/imageURL.js
@@ -1,7 +1,6 @@
 /* export const IMAGE_URL = "http://localhost:3000/rooms";
-export const IMAGE_URL_USERS = "http://localhost:3000/users";
-export const IMAGE_URL_MENU = "http://localhost:5173/public"; */
+export const IMAGE_URL_USERS = "http://localhost:3000/users";*/
+export const IMAGE_URL_MENU = "http://localhost:5173/public";
 
 export const IMAGE_URL = "https://hotelio-fullstack.onrender.com/rooms";
 export const IMAGE_URL_USERS = "https://hotelio-fullstack.onrender.com/users";
-export const IMAGE_URL_MENU = "https://hotelio-fullstack.onrender.com/public";
