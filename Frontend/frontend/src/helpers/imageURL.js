edea999129ac59906a9291aa0196db94719a4f1f
Daceman00 <dariomandic2000@gmail.com>
2025-07-20 11:23:20 +0200

    fix: update image URLs for menu and user resources to reflect production environment

--- Frontend/frontend/src/helpers/imageURL.js
+++ Frontend/frontend/src/helpers/imageURL.js
@@ -1,10 +1,11 @@
 // Localhost
 export const IMAGE_URL = "https://hotelio-bucket.s3.eu-north-1.amazonaws.com/rooms";
-export const IMAGE_URL_MENU = "http://localhost:5173/public/images";
+//export const IMAGE_URL_MENU = "http://localhost:5173/public/images";
+export const IMAGE_URL_MENU = "https://hoteliodario.netlify.app/images";
 export const IMAGE_URL_USERS = "https://hotelio-bucket.s3.eu-north-1.amazonaws.com/users/";
 
 
 // Netlify
 /* export const IMAGE_URL = "https://hotelio-fullstack.onrender.com/rooms";
-export const IMAGE_URL_MENU = "https://hoteliodario.netlify.app/images";
+
 export const IMAGE_URL_USERS = "https://hotelio-fullstack.onrender.com/users";  */
